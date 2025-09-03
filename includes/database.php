49c2ff7857ec17e43967ddf06cefb54d7044063c
Paulo Cesar Palacios Vargas <52548440+paulopv@users.noreply.github.com>
2025-09-03 19:02:53 +1000

    Update database.php

--- includes/database.php
+++ includes/database.php
@@ -1,11 +1,19 @@
 <?php
 
-$db = mysqli_connect('localhost', 'root', '', 'appsalon_mvc');
+$db = mysqli_connect(
+    $_ENV['DB_HOST'], 
+    $_ENV['DB_USER'], 
+    $_ENV['DB_PASS'], 
+    $_ENV['DB_NAME']);
+// echo "<pre>";
+// var_dump($db);
+// echo"</pre>";
 
+$db->set_charset('utf8');
 
-if (!$db) {
-    echo "Error: No se pudo conectar a MySQL.";
-    echo "errno de depuración: " . mysqli_connect_errno();
-    echo "error de depuración: " . mysqli_connect_error();
+if(!$db){
+    echo "ERROR: no se pudo conectar a MySQL.";
+    echo "errno de depuracion: " . mysqli_connect_errno();
+    echo "error de depuracion: " . mysqli_connect_error();
     exit;
-}
+} 
